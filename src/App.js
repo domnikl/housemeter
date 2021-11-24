87d8cac2c64fd054dd3947194b6efa5d94ba5e06
Hanni <johanna.liebler@gmail.com>
2021-11-24 10:13:48 +0100

    styling and background image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import ListHead from "./components/ListHead";
 import classes from "./App.module.css";
+import backgroundImage from './img/Colorful-Watercolor-Background-Graphics-8158369-1.jpg'
 
 function App() {
   return (
+    <div style={{ backgroundImage: `url(${backgroundImage})` }}>
     <div className={classes.App}>
       <header className={classes.AppHeader}>
         <p className={classes.p}>Housemeter</p>
         <ListHead />
       </header>
+    </div>
     </div>
   );
 }
