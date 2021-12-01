dd6a53728779a8b550b6375e3e4ea27bcbf3b545
Hanni <johanna.liebler@gmail.com>
2021-12-01 11:51:32 +0100

    clear code and rename Values

--- src/components/ListTable.js
+++ src/components/ListTable.js
@@ -1,19 +1,23 @@
-
+import React from "react";
 import classes from "./ListTable.module.css";
 import Value from "./Value";
+// import SelectMenu from "./SelectMenu";
 
 const ListTable = (props) => {
   return (
-    <table className={classes.container}>
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Type</th>
-          <th>Value</th>
-        </tr>
-      </thead>
+    <React.Fragment>
+      {/* <SelectMenu /> */}
+      <table className={classes.container}>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Type</th>
+            <th>Measurements</th>
+          </tr>
+        </thead>
+      </table>
       <Value />
-    </table>
+    </React.Fragment>
   );
 };
 
