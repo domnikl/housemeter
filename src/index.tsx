c626787f75cfd1803261803bb76835dce558c4a8
Hanni <johanna.liebler@gmail.com>
2022-06-17 15:46:35 +0200

    fix bug - dep react script

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
+const root = createRoot(document.getElementById("root")!!);
+
 root.render(
   <React.StrictMode>
     <App />
